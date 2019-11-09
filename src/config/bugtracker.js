01e7b81c851e9da4d08a56a6d1d64cfd98c77c39
zzeris <zzeris@gmail.com>
2019-11-09 16:55:34 -0300

    Add DotEnv

---
+++ src/config/bugtracker.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "type": "service_account",
+  "project_id": "resolute-future-258515",
+  "private_key_id": process.env.GOOGLE_KEY,
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDLiVC2aIABzF/W\nb4pcABkYz4MAnaXFZwGhcc6whLlX4jPIChSLEcf63JH5rxHIQEYELdAZkluHJlub\nFUtg07YXlYX0lfFe8esfwW5HtN/ep2+wIyHeFVSEno/VRsXzRPS8/EILhMmcKN6c\nFvsuWR4miVoe37ikYvHbPiq9XFQvak4XgmMLYvmCrF/IQXsr/rEB68PBKnnhDwEU\nXajlsb9qtX5Dj09LIBRDzAPjkjKBWGPbPoiQXhq11etK2Gyo9SmeH264i9+ANQF8\nWYDt0GUzbd30zddJ+6/8RnstG+PT58iXgvOdDAFtJoc57keanTB01ibLVKfwC795\nztc6ue6hAgMBAAECggEAS0R63BG18LRO5qI15MLGhSXL+LDTyjXG5RiMijtyvPyy\nEHIl2a3YKb1ioBZtOYQHQGlesO4uOcLmEmokB2kTSBJryKCgc2979zDVYuAQ/6ok\ng9riHdn+eNKYNxMmhclExhR1KgRPeBenBsr61U9wdmjJsmwiQGw0q1KIhKE9opCA\npZ2wlRZllGIajXumqhHkFF6sMQMXxhlVOCI0vWxqviPKPG2vsKXEY60I2qakhisk\nKlcXYFoUZBzaNwwTUduEbMJmZkuPpnW1nxEQKF7yLeJXlQvDgQHpYdz7gy1UHkqX\n/o3QxKbuwKKijw962h6pcwGyQ0GQS93pItBeA9YRbwKBgQDzeuxa7LzV4ZJhULXJ\ne2DDE3EJGscOcIf65FiPtBSLEQfRX2VqNpILZHtRgwabVKrjT0CNphJ85Xmxt7cY\niJQwBp8gkUWQtx2jF+4TkgJigJAmpaHbwJh08LdFpdwUKRFUOLMh2badf3b11H7Z\n3cd8bCLg2yvcseOAsz+msfqy9wKBgQDWAJZ+kLACMGnQaqVlzC42n5z+sI669hG6\npCfiPl4FIg2gNp01xd8RhidSNZ9bfCLkBNtSEj7DlHci0Vk8G1zz5+4K9GiqH6rR\nKBI4WqWgqkXYvLOMPJBN4Ei+mP3U25gn+iCLOTmrMO5g/pJ7ozQWn0OVEPGJyNY9\nPIAruHbtJwKBgFZ3bvyTWjRymTNqIETfRevBOcFL+ykLmZ7l/PV7tFf2wln+kNt5\nacAV/R1jdLMxDEcRXBFxytsD+4W7gpB54HZ1w4kjThsojKPg2bGlhqdIlOjEpYiP\nCxVsbwmS72dRdk/8mtEIqiugmrSOtEXW1wM+u9wyuY70taB9B7vBNRDdAoGBAJ48\ngwsE21BTEfrM0I3MAyPas9BTJsaxqnxSYzdUHFs5Sc/8chRPwliH1M1h3vpbeMUX\nnkA6Q3SEc+8n0w6xwPKMvSQWgsnAbqMHOSm4RR3eqZTQOwbnxWAABmoAYDgjQ4fT\n2Hp5jt25aZoIwrFvBNXxX5x6kc7q1RckhDVPyYgjAoGBANWctd4gmzPtoG0Xrr9s\nw4gwvItS2GcmiIt0hsGkBNSe8e+l/esF2mGfEfab5ZhDW+dha5/lrR3GE4ztOiKv\nMw6ahLmgy9dbyGSLjhgeaB1dGTVOY8f9yAcP4QY0qB+NFdisuOpq6bvQYn7+IPR1\ns3SXWiByNBv30QSt9/kGukrf\n-----END PRIVATE KEY-----\n",
+  "client_email": "bugtracker@resolute-future-258515.iam.gserviceaccount.com",
+  "client_id": "104823050189943332639",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/bugtracker%40resolute-future-258515.iam.gserviceaccount.com"
+}
